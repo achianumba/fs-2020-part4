27e8c192b045202740836ffb7807a83b47285e42
achianumba <chianumbaarinze@gmail.com>
2020-05-06 16:39:16 +0100

    all tests passed

--- tests/blogs-api.test.js
+++ tests/blogs-api.test.js
@@ -11,57 +11,50 @@ beforeEach(async () => {
   //clear database to ensure quality assurance
   await deleteAll();
   await closeDb();
+  await api.post("/api/blogs").send(blogs[0]);
+  await api.post("/api/blogs").send(blogs[1]);
+  await api.post("/api/blogs").send(blogs[2]);
 });
 
-//actual tests
-describe("when there are blogs in the database", () => {
-  test("the correct number of blogs are returned in json format", async () => {
-    const res = await api.get("/api/blogs");
-    expect(res.type).toMatch(/application\/json/);
-    expect(res.body).toHaveLength(3);
-  });
+test("The correct number of blogs are returned in json format", async () => {
+  const res = await api.get("/api/blogs");
+  expect(res.type).toMatch(/application\/json/);
+  expect(res.body).toHaveLength(3);
+});
 
-  test("id property of blogs are defined", async () => {
-    let res = await api.get("/api/blogs");
-    res.body.forEach(({ id }) => expect(id).toBeDefined());
-  });
+test("Each blog's id property is defined", async () => {
+  let res = await api.get("/api/blogs");
+  res.body.forEach(({ id }) => expect(id).toBeDefined());
+});
 
-  test("creating a new blog is successful and increases blog count", async () => {
+test("Missing 'likes' property return 0", async () => {
+  const missingLike = {
+    title: oneBlog.title,
+    author: oneBlog.author,
+    url: oneBlog.url,
+  };
+
+  const res = await api.post("/api/blogs").send(missingLike);
+  expect(res.body.likes).toBe(0);
+});
+
+describe("Authors can", () => {
+  test("Create a new blog which increments the blog count", async () => {
     let res = await api.post("/api/blogs").send(oneBlog);
     expect(res.body.title).toBe(oneBlog.title);
     let allBlogs = await api.get("/api/blogs");
     expect(allBlogs.body).toHaveLength(blogs.length + 1);
   });
 
-  test("missing 'likes' property return 0", async () => {
-    let missingLike = {
-      title: oneBlog.title,
-      author: oneBlog.author,
-      url: oneBlog.url,
-    };
-
-    let res = await api.post("/api/blogs").send(missingLike);
-    expect(res.body.likes).toBe(0);
-  });
-
-  test("creating a blog without the title and url properties returns status code 400", async () => {
-    let badRequest = {
-      url: oneBlog.url,
-    };
-
-    let res = await api.post("/api/blogs").send(badRequest);
-    expect(res.statusCode).toBe(400);
-  });
-
-  test("one can update a single blog", async () => {
+  test("Update a blog", async () => {
     const getBlogs = await api.get("/api/blogs"); //for efficiency, refactor this request only one blog post later.
-    await api
+    const update = await api
       .put(`/api/blogs/${getBlogs.body[0].id}`)
-      .send({ likes: 21 })
-      .expect(200);
+      .send({ likes: 21 });
+    expect(update.status).toBe(200);
   });
 
-  test("one can delete a single blog", async () => {
+  test("delete a blog", async () => {
     const getBlogs = await api.get("/api/blogs"); //for efficiency, refactor this request only one blog post later.
     await api.del(`/api/blogs/${getBlogs.body[0].id}`).expect(204);
   });
